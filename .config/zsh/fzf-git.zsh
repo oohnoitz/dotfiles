49f0951b0ec21583e962a814b77027a53448f4cb
Hung Tran <hung@0x0.ninja>
2018-09-29 08:31:42 -0500

    zsh: update fzf git checkout branch function

--- .config/zsh/fzf-git.zsh
+++ .config/zsh/fzf-git.zsh
@@ -32,11 +32,14 @@ gcb() {
   result=$(git branch -a --color=always | grep -v '/HEAD\s' | sort |
     fzf-down --ansi --multi --tac --preview-window right:70% \
       --preview 'git log --oneline --graph --date=short --pretty="format:%C(auto)%cd %h%d %s" $(sed s/^..// <<< {} | cut -d" " -f1) | head -'$LINES |
-    sed 's/^..//' | cut -d' ' -f1 |
-    sed 's#^remotes/##')
+    sed 's/^..//' | cut -d' ' -f1)
 
   if [[ $result != "" ]]; then
-    git checkout "$result"
+    if [[ $result == remotes/* ]]; then
+      git checkout --track $(echo $result | sed 's#remotes/##')
+    else
+      git checkout "$result"
+    fi
   fi
 }
 
