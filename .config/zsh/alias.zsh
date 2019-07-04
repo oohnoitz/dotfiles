e76fc9d6797363466bd966c7fbceb213c613b87b
Hung Tran <hung@0x0.ninja>
2019-07-03 19:06:29 -0500

    zsh: add ninja build alias

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,1 +1,3 @@
 alias v=nvim
+alias nbi='rm -rf build && meson build && ninja -C build && sudo ninja -C build install'
+alias nbisys='rm -rf build && meson build -Dsysconfdir=/etc && ninja -C build && sudo ninja -C build install'
