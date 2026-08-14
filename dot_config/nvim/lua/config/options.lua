a289ff599738a27da01f305855266330ac422d5f
Hung Tran <hungtran7@proton.me>
2026-08-14 14:37:00 +0000

    nvim: update clipboard

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -8,3 +8,22 @@ opt.foldcolumn = "1"
 opt.foldlevel = 99
 opt.foldlevelstart = 99
 opt.foldenable = true
+
+if not vim.env.SSH_CONNECTION then
+  vim.opt.clipboard = "unnamedplus"
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = function()
+        return { vim.fn.split(vim.fn.getreg('"'), "\n", true), vim.fn.getregtype('"') }
+      end,
+      ["*"] = function()
+        return { vim.fn.split(vim.fn.getreg('"'), "\n", true), vim.fn.getregtype('"') }
+      end,
+    },
+  }
+end
